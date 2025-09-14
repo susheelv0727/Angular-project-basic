02827c5a33bab5fd72d0239932464541f95de7fe
susheelv0727 <susheelv07@gmail.com>
2025-09-14 14:08:13 -0500

    include ngif back

--- src/app/form.component.ts
+++ src/app/form.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { ReactiveFormsModule, FormBuilder, Validators } from '@angular/forms';
-import { JsonPipe } from '@angular/common';
+import { JsonPipe, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-form',
   standalone: true,
-  imports: [ReactiveFormsModule, JsonPipe],
+  imports: [ReactiveFormsModule, JsonPipe, NgIf],
   template: `
     <h2>Reactive Form Example</h2>
     <form [formGroup]="form" (ngSubmit)="onSubmit()">
